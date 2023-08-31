3379669967d12518be06dcfbfd2732c69db6df32
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-31 17:51:28 -0400

    flyover times

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
 // fetchCoordsByIP('99.255.27.90', (error, coordinates) => {
 //   if (error) {
@@ -16,4 +16,15 @@ fetchMyIP((error, ip) => {
 //   }
 
 //   console.log("It worked! Returned coordinates:", coordinates);
+// });
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
 // });
